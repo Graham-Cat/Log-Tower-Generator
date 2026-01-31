baab50f4cf98a0d759a0a1a0f1f0db127a12773a
Christopher Feick <iamnotreallyacat@gmail.com>
2026-01-31 03:14:13 -0500

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,12 @@
+"""
+Log Tower Generator Library
+---------------------------
+Exposes the main generator class for calculating Log-Tower polynomials P(A_n) and P(R_n).
+"""
 
+# Expose the main class directly to the package level
+from .generator import LogTowerGenerator
+
+# Package Metadata
+__version__ = "1.0.0"
+__all__ = ["LogTowerGenerator"]
